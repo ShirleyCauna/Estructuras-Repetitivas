ca487124e24b2fbfe3ef4dd85510f558c0bded25
ShirleyCauna <caunashirley11@gmail.com>
2025-05-29 23:22:03 -0500

    docs: agrega comentarios explicativos al ejercicio 2

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main(){
 	int num;
+    // Repetir la lectura mientras el número ingresado sea mayor o igual a 0
     do {
+        // Solicita al usuario que ingrese un número
         cout << "Ingrese un numero: ";
         cin >> num;
-    } while (num >= 0);
+    } while (num >= 0); // Si el número es negativo, termina el ciclo
 
+    // Mensaje de finalización
     cout << "Fin de la captura" << endl;
 	return 0;
 }
